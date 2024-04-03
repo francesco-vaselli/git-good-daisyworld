c698219ac40f331e265838736927f2cc9cbe8fe3
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2024-04-03 14:27:32 +0200

    Add print to main function

--- main.cpp
+++ main.cpp
@@ -10,8 +10,14 @@ int main() {
   World world(size, start_black_percentage, start_white_percentage, max_age);
   std::cout << "Initial global temperature: " << world.global_temperature()
             << '\n';
+  world.print();
+
+  std::cin.get();
+  std::cout << "\033c";
+
   world.step(1.);
   std::cout << "Global temperature after one time step: "
             << world.global_temperature() << '\n';
+  world.print();
   return 0;
 }
